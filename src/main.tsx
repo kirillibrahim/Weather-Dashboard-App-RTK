29d95e3190e98383f65b08b0c4d4f999ca616972
kirillibrahim <kirillbrah@gmail.com>
2024-09-03 22:59:32 +0300

    Initial setup for Redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
